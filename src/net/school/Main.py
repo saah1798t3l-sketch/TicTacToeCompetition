f066f926a40eab1be45a66a270c069118dba6603
Biff <saah1798t3l@gmail.com>
2025-09-27 19:17:43 -0400

    Made the octothorp for the game board, made a drawX() function to draw a red x at a specified location

--- src/net/school/Main.py
+++ src/net/school/Main.py
@@ -1,7 +1,7 @@
 from enum import Enum
 import os
 import pygame
-import sys as System
+import sys
 from pygame import QUIT
 
 class Symbol(Enum):
@@ -14,14 +14,29 @@ class Spot():
     def updateSymbol(this, symbol):
         this.Symbol = symbol
 
+def makeGameLines(x):
+    pygame.draw.line(window, (0,0,0),(x,0),(x,800),100)
+    pygame.draw.line(window, (0,0,0),(0,x), (800, x), 100)
+
+def drawX(x,y):
+    pygame.draw.line(window, (255,0,0),(x-100,y-100),(x+100,y+100),1)
+    pygame.draw.line(window, (255,0,0),(x+100,y-100),(x-100,y+100),1)
+
+
 
 pygame.init()
-pygame.display.set_mode((1000,800), 0 ,32)
+window = pygame.display.set_mode((800,800), 0 ,32)
 pygame.display.set_caption("Tic Tac Toe")
 pygame.display.set_icon(pygame.image.load(os.path.join("..","..","..","resources","ticTacToe.png")))
+window.fill((255,255,255))
+
+makeGameLines(250)
+makeGameLines(550)
+drawX(100,100)
+
 pygame.display.update()
 while True:
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
-            System.exit()
+            sys.exit()
